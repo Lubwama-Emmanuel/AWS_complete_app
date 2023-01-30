01606a7581288ad29b702edd001743988b65eed1
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2023-01-30 19:43:09 +0300

    feat:(body validation)

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,18 +1,44 @@
-const dynamoDB = require('../database/db')
+const dynamoDB = require("../database/db");
 const tableName = "AuctionTable-dev";
+const { queryStringSchema } = require("../lib/schemas/getAuctionsSchema");
+const queryString = require("querystring");
 
-module.exports.getAuctions = async (event, context) => {
-    try {
-      const auctions = await dynamoDB.scan({ TableName: tableName }).promise();
+module.exports.getAuctions = async (event) => {
+  try {
+    const { status } = event.queryStringParameters;
+    const queryParams = queryString.parse(event.queryStringParameters);
+    const { error } = queryStringSchema.validate(queryParams);
+
+    if (error) {
+      console.log(error);
       return {
-        statusCode: 200,
-        body: JSON.stringify(auctions),
-      };
-    } catch (err) {
-      console.log("AN ERROR OCCURED", err);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(err),
+        statusCode: 400,
+        body: JSON.stringify(error),
       };
     }
-  };
+
+    const params = {
+      TableName: tableName,
+      IndexName: "statusAndEndDate",
+      KeyConditionExpression: "#status = :status",
+      ExpressionAttributeValues: {
+        ":status": status,
+      },
+      ExpressionAttributeNames: {
+        "#status": "status",
+      },
+    };
+
+    const auctions = await dynamoDB.query(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(auctions),
+    };
+  } catch (err) {
+    console.log("AN ERROR OCCURED", err);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(err),
+    };
+  }
+};
